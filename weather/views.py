bd8c26c522cd9cb790cf75c139a5daab630f6666
Didisimmons <onyema.onyejekwe@gmail.com>
2022-05-27 00:40:41 +0000

    add weather app, templates and static files

--- weather/views.py
+++ weather/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def index(request):
+    url = "https://api.openweathermap.org/data/2.5/weather?q={},{}&appid={}"
+    return render(request,'weather/index.html')
--- weather_app/settings.py
+++ weather_app/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -56,7 +56,8 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            
+            os.path.join(BASE_DIR, 'templates'),
+            os.path.join(BASE_DIR, 'templates', 'allauth'),
         ],
         'APP_DIRS': True,
         'OPTIONS': {
