95cd9ba124138cd919aa05f2342f7bf312e9e044
Didisimmons <onyema.onyejekwe@gmail.com>
2022-05-31 03:39:02 +0000

    add allowed hosts

--- weather_app/settings.py
+++ weather_app/settings.py
@@ -23,10 +23,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 
 SECRET_KEY = os.environ.get('SECRET_KEY')
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://weathertabs.herokuapp.com']
+ALLOWED_HOSTS = ['https://weathertabs.herokuapp.com', 'localhost']
 
 
 # Application definition
