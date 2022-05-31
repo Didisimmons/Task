9c84a9db44dbccb2e356745dc28a885b71c0b973
Didisimmons <onyema.onyejekwe@gmail.com>
2022-05-31 03:32:19 +0000

    fixed allowed host and add update settings.py

--- weather_app/settings.py
+++ weather_app/settings.py
@@ -21,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--8-5teuz=-rg^&7ipmnm8t*vc5+u1&d@!yy=9-x@^bps+^2hz*'
 
+SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://weathertabs.herokuapp.com']
 
 
 # Application definition
