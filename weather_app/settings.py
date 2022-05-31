06992ebe9495542ebf74d31e36075b1c7ac37ef8
Didisimmons <onyema.onyejekwe@gmail.com>
2022-05-31 03:48:59 +0000

    add database url

--- weather_app/settings.py
+++ weather_app/settings.py
@@ -80,10 +80,7 @@ WSGI_APPLICATION = 'weather_app.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
 }
 
 
