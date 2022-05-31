46b92678180254bab868a47b8946a35ca1d397f2
Didisimmons <onyema.onyejekwe@gmail.com>
2022-05-31 11:27:59 +0000

    disable collectstatic and add whitenoise

--- weather_app/settings.py
+++ weather_app/settings.py
@@ -92,7 +92,7 @@ else:
         }
     }
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
@@ -130,12 +130,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = BASE_DIR / "staticfiles"
 STATIC_URL = '/static/'
 
 # This informs django were are static files are located
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 API_KEY = os.environ.get('API_KEY')
 
