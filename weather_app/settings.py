6ca5a619108df5b7ff49cbd94c7b928655269a20
Didisimmons <onyema.onyejekwe@gmail.com>
2022-06-01 06:39:19 +0000

    set debug to false

--- weather_app/settings.py
+++ weather_app/settings.py
@@ -16,7 +16,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
@@ -29,8 +28,6 @@ DEBUG = False
 
 
 ALLOWED_HOSTS = [os.environ.get('HEROKU_HOSTNAME', 'localhost')]
-# ALLOWED_HOSTS = ['198.211.99.20', 'localhost', '127.0.0.1']
-# Application definition
 
 INSTALLED_APPS = [
     'django.contrib.admin',
